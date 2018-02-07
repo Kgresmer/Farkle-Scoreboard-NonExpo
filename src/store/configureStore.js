caab75861307fcd699a417b43b370377258b9780
kgresmer <kevin_gresmer@optum.com>
2018-02-07 13:03:15 -0600

    test changes to async store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,14 +2,14 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 import {persistReducer, persistStore} from "redux-persist";
-import storage from 'redux-persist/lib/storage';
+import { AsyncStorage } from 'react-native'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const persistConfig = {
     key: 'root',
-    storage: storage,
+    storage: AsyncStorage,
     stateReconciler: autoMergeLevel2,
-    blacklist: ['sortOrder']
+    whitelist: ['player']
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
